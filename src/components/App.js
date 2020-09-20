937337e63ca9ca69881fa2095c101f2db83665d5
jonghyun <jonghyeon2046@gmail.com>
2020-09-20 17:09:52 +0900

    App.js edit

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import { authService } from "../myFirebase";
 
 function App() {
     const [init, setInit] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [userObj, setUserObj] = useState(null);
     useEffect(() => {
         authService.onAuthStateChanged((user) => {
+            //로그인 로그아웃 & 초기화 될 때 발동 된다
             if (user) {
-                setIsLoggedIn(true);
+                setUserObj(user);
             } else {
-                setIsLoggedIn(false);
             }
             setInit(true);
         });
@@ -18,7 +18,7 @@ function App() {
     return (
         <>
             {init ? (
-                <AppRouter isLoggedIn={isLoggedIn} />
+                <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
             ) : (
                 "Initializing ....."
             )}
